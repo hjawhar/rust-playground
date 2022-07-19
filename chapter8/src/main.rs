06a29fc14aa8247b6eb9fbf5397257d935f7888d
Hassan Jawhar <hassanjawhar@protonmail.com>
2022-07-19 12:51:05 +0300

    Added chapter8

---
+++ chapter8/src/main.rs
@@ -0,0 +1,68 @@
+use std::collections::HashMap;
+fn main() {
+    println!("Hello, world!");
+
+    let v = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v[2];
+    println!("The third element is {}", third);
+
+    match v.get(2) {
+        Some(third) => println!("The third element is {}", third),
+        None => println!("There is no third element."),
+    }
+
+    let v = vec![100, 32, 57];
+    for i in &v {
+        println!("{}", i);
+    }
+
+    let mut v = vec![100, 32, 57];
+    v.push(69);
+
+    for i in &mut v {
+        *i += 50;
+        println!("{}", i);
+    }
+
+    println!("-----------------------");
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
+
+    println!("-----------------------");
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+
+    let mut scores: HashMap<_, _> = teams.into_iter().zip(initial_scores.into_iter()).collect();
+
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
+
+    println!("-----------------------");
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    for (key, value) in &map {
+        println!("{}: {}", key, value);
+    }
+
+    println!("-----------------------");
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
+}
